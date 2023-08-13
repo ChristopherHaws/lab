a0d0f6259ffb4c542438358e519a538619a7435f
Christopher Haws <cribs2@gmail.com>
2023-08-13 15:53:28 -0700

    Both samples are working

---
+++ vite-02-monorepo-monoproj/apps/frontend/vite.config.ts
@@ -0,0 +1,14 @@
+import { resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
+const currentDir = fileURLToPath(new URL('.', import.meta.url));
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	//FIXME: Is this correct? I have a feeling this should be the workspace root, not the current package root.
+	root: resolve(currentDir),
+	plugins: [react()],
+});
